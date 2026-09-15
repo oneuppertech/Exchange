f2e26a054aeb88d79e43512d844df6a10f6cbcdd
windihotspot <boiwindihotspot@gmail.com>
2026-09-15 11:43:38 -0400

    deployment

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-import LandingPage from '@/views/LandingPage.vue'
-import Onboarding from '@/views/Onboarding.vue'
-
-import StepProfile from '@/views/StepProfile.vue'
-import StepIdentity from '@/views/StepIdentity.vue'
-import Stepaddress from '@/views/Stepaddress.vue'
-import Stepverify from '@/views/Stepverify.vue'
 import AdminExchange from '@/views/AdminExchange.vue'
-import CustomerExchange from '@/views/CustomerExchange.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,56 +8,10 @@ const router = createRouter({
     // Landing page
     {
       path: '/',
-      name: 'customer',
-      component: CustomerExchange,
-    },
-    {
-      path: '/landing',
-      name: 'landing',
-      component: LandingPage,
-    },
-    {
-      path: '/admin',
-      name: 'admin',
+      name: 'exchange',
       component: AdminExchange,
-    },
-
-    // Onboarding
-    {
-      path: '/',
-      component: Onboarding,
-
-      children: [
-        {
-          path: '',
-          redirect: { name: 'onboarding-profile' },
-        },
-
-        {
-          path: 'onboarding-profile',
-          name: 'onboarding-profile',
-          component: StepProfile,
-        },
-
-        {
-          path: 'onboarding-identity',
-          name: 'onboarding-identity',
-          component: StepIdentity,
-        },
-
-        {
-          path: 'onboarding-address',
-          name: 'onboarding-address',
-          component: Stepaddress,
-        },
-
-        {
-          path: 'onboarding-verify',
-          name: 'onboarding-verify',
-          component: Stepverify,
-        },
-      ],
-    },
+    }
+   
   ],
 })
 
